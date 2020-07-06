c6f00af899d3446bc5bacb65dd59b2b8fdb74fa9
anyahirota <anyahirota@gmail.com>
2020-07-06 13:50:32 -0700

    Edit user model

--- w6d1/aa-questions/user.rb
+++ w6d1/aa-questions/user.rb
@@ -5,8 +5,50 @@ require_relative 'question_like'
 require_relative 'reply'
 
 class User
-    def self.find_by_id
-        data = QuestionsDatabase.instance.execute('SELECT * FROM users')
-        data.map {|datum| User.new(datum)}
+    def self.find_by_id(id)(<<-SQL, id: id)
+      SELECT
+        users.*
+      FROM
+        users
+      WHERE
+        users.id = :id
+    SQL
+
+    user_data.nil? ? nil : User.new(user_data)
+    end
+        # data = QuestionsDatabase.instance.execute('SELECT * FROM users')
+        # data.map {|datum| User.new(datum)}
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
     end 
+
+#     def attrs
+#     { fname: fname, lname: lname }
+#   end
+
+#   def save
+#     if @id
+#       QuestionsDatabase.execute(<<-SQL, attrs.merge({ id: id }))
+#         UPDATE
+#           users
+#         SET
+#           fname = :fname, lname = :lname
+#         WHERE
+#           users.id = :id
+#       SQL
+#     else
+#       QuestionsDatabase.execute(<<-SQL, attrs)
+#         INSERT INTO
+#           users (fname, lname)
+#         VALUES
+#           (:fname, :lname)
+#       SQL
+
+#       @id = QuestionsDatabase.last_insert_row_id
+#     end
+#     self
+#   end
 end 
