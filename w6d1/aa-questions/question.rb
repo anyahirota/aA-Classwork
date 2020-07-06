634ba81d1eb917ed23f72bc85c724f5ada4a2738
anyahirota <anyahirota@gmail.com>
2020-07-06 15:30:29 -0700

    Finish #initialize, ::all, ::find_by_id and attr_accessors for Question class

--- w6d1/aa-questions/question.rb
+++ w6d1/aa-questions/question.rb
@@ -5,5 +5,30 @@ require_relative 'question_like'
 require_relative 'reply'
 
 class Question
- 
+    attr_accessor :id, :title, :body, :author_id
+
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map {|datum| Question.new(datum)}
+    end 
+
+    def self.find_by_id(id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                id = ?
+        SQL
+        return nil unless data.length > 0 
+        Question.new(data.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end 
 end 
